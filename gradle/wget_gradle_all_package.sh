2cf2f1730e2d4243b981a6f366c86ffda62263a4
王智 <z.wang@tianrang-inc.com>
2020-07-30 15:53:39 +0800

    update setup gradle scripts

--- gradle/wget_gradle_all_package.sh
+++ gradle/wget_gradle_all_package.sh
@@ -7,12 +7,16 @@ set -o pipefail
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_PATH=$SCRIPT_PATH/..
 
+FORCE_RE_SETUP=$1
+
 PKG_PATH=$PROJECT_PATH/gradle/pkg
 mkdir -p $PKG_PATH
 GRADLE_ALL_PACKAGE=$PKG_PATH/gradle-6.1.1-all.zip
+if [[ "true" == $FORCE_RE_SETUP ]]; then
+    rm -f $GRADLE_ALL_PACKAGE
+fi
 if [[ -f "$GRADLE_ALL_PACKAGE" ]]; then
     echo "$GRADLE_ALL_PACKAGE already exists"
 else
-    rm -f $GRADLE_ALL_PACKAGE
     wget -O $GRADLE_ALL_PACKAGE https://services.gradle.org/distributions/gradle-6.1.1-all.zip
 fi
