4ddf22b5d612975a0d8c1179693340c0e9dba290
王智 <z.wang@tianrang-inc.com>
2020-07-30 17:48:31 +0800

    re-construct k8s_setup

--- k8s_setup/src/main/java/tech/geekcity/blackhole/k8s_setup/Application.java
+++ k8s_setup/src/main/java/tech/geekcity/blackhole/k8s_setup/Application.java
@@ -1,13 +1,18 @@
-package tech.geekcity.blackhole.k8s.setup;
+package tech.geekcity.blackhole.k8s_setup;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     @Override
     public void run(String... args) {
+        LOGGER.info("running with arguments: {}", String.join(" ", args));
     }
 
     public static void main(String[] args) {
