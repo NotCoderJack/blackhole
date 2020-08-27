07f53296ea32389dbc9c07e8df9d162e444c885e
ben.wangz <ben.wangz@foxmail.com>
2020-08-27 12:06:15 +0800

    support ssh commander

--- ssh/src/main/java/tech/geekcity/blackhole/ssh/SshCommander.java
+++ ssh/src/main/java/tech/geekcity/blackhole/ssh/SshCommander.java
@@ -3,13 +3,27 @@ package tech.geekcity.blackhole.ssh;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ClientChannel;
+import org.apache.sshd.client.channel.ClientChannelEvent;
+import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
+import org.apache.sshd.client.keyverifier.DefaultKnownHostsServerKeyVerifier;
+import org.apache.sshd.client.session.ClientSession;
 import org.inferred.freebuilder.FreeBuilder;
+import tech.geekcity.blackhole.core.Configurable;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.security.KeyPair;
+import java.util.EnumSet;
 
 @FreeBuilder
 @JsonDeserialize(builder = SshCommander.Builder.class)
-public abstract class SshCommander {
+public abstract class SshCommander implements Configurable {
+    private transient SshClient client;
+    private transient ClientSession session;
+
     /**
      * Returns a new {@link Builder} with the same property values as this {@link SshCommander}
      */
@@ -24,6 +38,11 @@ public abstract class SshCommander {
 
         public static Builder newInstance() {
             return new Builder();
+        }
+
+        public Builder() {
+            standardOutput(new ByteArrayOutputStream());
+            errorOutput(new ByteArrayOutputStream());
         }
 
         public String toJson() throws JsonProcessingException {
@@ -41,5 +60,54 @@ public abstract class SshCommander {
         public SshCommander parseFromJson(String json) throws IOException {
             return objectMapper.readValue(json, SshCommander.class);
         }
+    }
+
+    public abstract String username();
+
+    public abstract String host();
+
+    public abstract int port();
+
+    public abstract KeyPair keyPair();
+
+    public abstract OutputStream standardOutput();
+
+    public abstract OutputStream errorOutput();
+
+    @Override
+    public void open() throws IOException {
+        client = SshClient.setUpDefaultClient();
+        client.setServerKeyVerifier(new DefaultKnownHostsServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE));
+        client.start();
+        session = client.connect(username(), host(), port())
+                .verify()
+                .getSession();
+        session.addPublicKeyIdentity(keyPair());
+        session.auth().verify();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (null != client) {
+            client.stop();
+            client.close();
+        }
+        if (null != session) {
+            session.close();
+        }
+    }
+
+    public int run(String command) throws IOException {
+        return run(command, 0L);
+    }
+
+    public int run(String command, long timeoutInMilliseconds) throws IOException {
+        try (ClientChannel channel = session.createExecChannel(command)) {
+            channel.setOut(System.out);
+            channel.setErr(System.err);
+            channel.open().verify();
+            channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED), timeoutInMilliseconds);
+        }
+        return 0;
     }
 }
--- ssh/src/test/java/tech/geekcity/blackhole/ssh/TestSsh.java
+++ ssh/src/test/java/tech/geekcity/blackhole/ssh/TestSsh.java
@@ -1,40 +1,29 @@
 package tech.geekcity.blackhole.ssh;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.sshd.client.SshClient;
-import org.apache.sshd.client.channel.ClientChannel;
-import org.apache.sshd.client.channel.ClientChannelEvent;
-import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
-import org.apache.sshd.client.keyverifier.DefaultKnownHostsServerKeyVerifier;
-import org.apache.sshd.client.session.ClientSession;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.security.KeyPair;
-import java.util.EnumSet;
 
 public class TestSsh {
     @Test
     public void test() throws IOException, ClassNotFoundException {
         byte[] keyPairDataBytes = FileUtils.readFileToByteArray(new File("../dist/dim/sshd/id_rsa.key.pair"));
         KeyPair keyPair = RsaKeyPairWrap.deserialize(keyPairDataBytes).keyPair();
-        SshClient client = SshClient.setUpDefaultClient();
-        client.setServerKeyVerifier(new DefaultKnownHostsServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE));
-        client.start();
-        try (ClientSession session = client.connect("root", "localhost", 2222)
-                .verify()
-                .getSession()) {
-            session.addPublicKeyIdentity(keyPair);
-            session.auth().verify();
-            try (ClientChannel channel = session.createExecChannel("hostname")) {
-                channel.setOut(System.out);
-                channel.setErr(System.err);
-                channel.open().verify();
-                channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED), 0L);
-            }
+
+        try (SshCommander sshCommander = SshCommander.Builder.newInstance()
+                .username("root")
+                .host("localhost")
+                .port(2222)
+                .keyPair(keyPair)
+                .standardOutput(System.out)
+                .errorOutput(System.err)
+                .build()) {
+            sshCommander.open();
+            sshCommander.run("hostname");
+            sshCommander.run("hostname -i");
         }
-        client.stop();
-        client.close();
     }
 }
