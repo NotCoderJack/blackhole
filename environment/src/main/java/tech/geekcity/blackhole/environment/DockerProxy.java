b1353d6456a79c29777a568d905fa92163660ffe
ben.wangz <ben.wangz@foxmail.com>
2021-02-09 22:46:01 +0800

    add build image to proxy: lib bug not yet solved

--- environment/src/main/java/tech/geekcity/blackhole/environment/DockerProxy.java
+++ environment/src/main/java/tech/geekcity/blackhole/environment/DockerProxy.java
@@ -3,6 +3,9 @@ package tech.geekcity.blackhole.environment;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.BuildImageCmd;
+import com.github.dockerjava.api.command.BuildImageResultCallback;
 import com.github.dockerjava.api.command.VersionCmd;
 import com.github.dockerjava.api.model.Version;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
@@ -10,16 +13,20 @@ import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
+import com.google.common.base.Preconditions;
 import org.inferred.freebuilder.FreeBuilder;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 
 @FreeBuilder
 @JsonDeserialize(builder = DockerProxy.Builder.class)
 public abstract class DockerProxy implements Closeable {
-    private transient DockerClientConfig dockerClientConfig;
-    private transient DockerHttpClient httpClient;
+    private transient DockerClient dockerClient;
 
     /**
      * Returns a new {@link Builder} with the same property values as this {@link DockerProxy}
@@ -54,24 +61,57 @@ public abstract class DockerProxy implements Closeable {
         }
     }
 
-    public String apiVersion() {
-        VersionCmd versionCmd = DockerClientImpl.getInstance(dockerClientConfig, httpClient)
-                .versionCmd();
-        Version version = versionCmd.exec();
-        return version.getApiVersion();
-    }
-
     public void open() {
-        dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
+        DockerClientConfig dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
                 .build();
-        httpClient = new ApacheDockerHttpClient.Builder()
+        DockerHttpClient dockerHttpClient = new ApacheDockerHttpClient.Builder()
                 .dockerHost(dockerClientConfig.getDockerHost())
                 .sslConfig(dockerClientConfig.getSSLConfig())
                 .build();
+        dockerClient = DockerClientImpl.getInstance(dockerClientConfig, dockerHttpClient);
     }
 
     @Override
     public void close() throws IOException {
 
+    }
+
+    public String apiVersion() {
+        VersionCmd versionCmd = dockerClient.versionCmd();
+        Version version = versionCmd.exec();
+        return version.getApiVersion();
+    }
+
+    public void buildImage(
+            @Nonnull File dockerFile,
+            @Nullable File baseDirectory,
+            @Nonnull String imageName,
+            @Nonnull String tag
+    ) {
+        // TODO there is a bug with docker-java:
+        // https://github.com/docker-java/docker-java/issues/1537
+        // dockerfile should in baseDirectory
+        Preconditions.checkArgument(
+                null == baseDirectory || dockerFile.getParentFile().equals(baseDirectory),
+                "there is a bug with docker-java lib: %s",
+                "https://github.com/docker-java/docker-java/issues/1537");
+        BuildImageCmd buildImageCmd = dockerClient.buildImageCmd()
+                .withTags(Collections.singleton(String.format("%s:%s", imageName, tag)))
+                .withBaseDirectory(null != baseDirectory ? baseDirectory : dockerFile.getParentFile())
+                .withDockerfile(dockerFile);
+        String imageId = buildImageCmd.exec(new BuildImageResultCallback()).awaitImageId();
+        System.out.println(imageId);
+    }
+
+    public void startContainer(String imageNameWithTag, String containerName) {
+
+    }
+
+    public void stopContainer(String containerName) {
+
+    }
+
+    public String exec(String containerName, String command) {
+        return "response";
     }
 }
--- environment/src/test/java/tech/geekcity/blackhole/environment/DockerProxyTest.java
+++ environment/src/test/java/tech/geekcity/blackhole/environment/DockerProxyTest.java
@@ -1,19 +1,58 @@
 package tech.geekcity.blackhole.environment;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 public class DockerProxyTest {
+    private DockerProxy dockerProxy;
+
+    @BeforeEach
+    void setUp() {
+        dockerProxy = DockerProxy.Builder.newInstance()
+                .build();
+        dockerProxy.open();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        dockerProxy.close();
+    }
+
     @Test
     public void testApiVersion() {
-        try (DockerProxy dockerProxy = DockerProxy.Builder.newInstance()
-                .build()) {
-            dockerProxy.open();
-            Assertions.assertTrue(dockerProxy.apiVersion().compareTo("1.39") >= 0);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Assertions.assertTrue(dockerProxy.apiVersion().compareTo("1.39") >= 0);
+    }
+
+    @Test
+    public void testBuildImage() throws IOException {
+        String content = IOUtils.toString(
+                Objects.requireNonNull(
+                        getClass().getClassLoader()
+                                .getResourceAsStream("build_image.dockerfile")),
+                StandardCharsets.UTF_8);
+        LoggerFactory.getLogger(getClass()).debug(content);
+        Path dockerFile = Files.createTempFile("build_image.", ".dockerfile");
+        dockerFile.toFile().deleteOnExit();
+        FileUtils.writeStringToFile(dockerFile.toFile(), content, StandardCharsets.UTF_8);
+//        Path dockerDirectory = Files.createTempDirectory("docker");
+        FileUtils.copyFile(dockerFile.toFile(),
+                new File(String.format(
+                        "%s/build_image.dockerfile",
+                        dockerFile.toFile().getParentFile().getAbsolutePath())));
+        // TODO see implementation of buildImage: baseDirectory bug
+        dockerProxy.buildImage(dockerFile.toFile(), null, "test_base_docker", "v0.1");
+//        FileUtils.deleteDirectory(dockerDirectory.toFile());
     }
 }
